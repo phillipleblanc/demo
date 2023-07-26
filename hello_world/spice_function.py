9a03a383e8ec021c2cf26761c97bf322fb904c96
Phillip LeBlanc <phillip@leblanc.tech>
2023-07-26 17:27:48 +0900

    Create spice_function.py

---
+++ hello_world/spice_function.py
@@ -0,0 +1,10 @@
+def process(context: dict, 
+            duckdb: duckdb.DuckDBPyConnection, 
+            spice_client: spicepy.Client):
+  # Temporary step
+  duckdb.sql("""CREATE TABLE IF NOT EXISTS output.hello_world (
+    block_number BIGINT,
+    greeting TEXT
+    )""")
+
+  duckdb.sql(f"INSERT INTO output.hello_world VALUES ({context['block_number']}, 'Hello!'")
