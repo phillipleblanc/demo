45dbd02034a13d4d6b53396fe42c4e90f7b0ac7f
Phillip LeBlanc <phillip@leblanc.tech>
2023-07-27 11:33:07 +0900

    Update spice_function.py

--- hello_world/spice_function.py
+++ hello_world/spice_function.py
@@ -1,5 +1,6 @@
 import duckdb
 import spicepy
+import web3
 
 def process(context: dict, 
             duckdb: duckdb.DuckDBPyConnection, 
@@ -9,6 +10,9 @@ def process(context: dict,
     block_number BIGINT,
     greeting TEXT
     )""")
+  api_key = os.environ["SPICE_API_KEY"]
+  w3 = Web3(Web3.HTTPProvider(f"https://data.spiceai.io/eth?api_key={api_key}"))
+  print(f"The latest block number is {w3.eth.get_block_number()}")
 
   duckdb.sql(f"INSERT INTO output.hello_world VALUES ({context['block_number']}, 'Hello!')")
 
