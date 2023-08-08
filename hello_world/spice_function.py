0507b3fcb51782f939f0ba1304a73aa2b68db9bf
Phillip LeBlanc <phillip@leblanc.tech>
2023-08-09 00:04:52 +0900

    Remove temp creation

--- hello_world/spice_function.py
+++ hello_world/spice_function.py
@@ -6,11 +6,6 @@ import os
 def process(context: dict, 
             duckdb: duckdb.DuckDBPyConnection, 
             spice_client: spicepy.Client):
-  # Temporary step
-  duckdb.sql("""CREATE TABLE IF NOT EXISTS output.hello_world (
-    block_number BIGINT,
-    greeting TEXT
-    )""")
   api_key = os.environ["SPICE_API_KEY"]
   w3 = Web3(Web3.HTTPProvider(f"https://data.spiceai.io/eth?api_key={api_key}"))
   print(f"The latest block number is {w3.eth.get_block_number()}")
