54b88b1ac33385f305353c820424ea6e27dea6c9
Phillip LeBlanc <phillip@leblanc.tech>
2023-08-09 00:08:03 +0900

    Add spice query

--- hello_world/spice_function.py
+++ hello_world/spice_function.py
@@ -8,7 +8,10 @@ def process(context: dict,
             spice_client: spicepy.Client):
   api_key = os.environ["SPICE_API_KEY"]
   w3 = Web3(Web3.HTTPProvider(f"https://data.spiceai.io/eth?api_key={api_key}"))
-  print(f"The latest block number is {w3.eth.get_block_number()}")
+  print(f"The latest block number from the node is {w3.eth.get_block_number()}")
+
+  df = spice_client.query("SELECT MAX(number) as max FROM eth.recent_blocks").read_pandas()
+  print(f"The latest block number from Spice is {df['max'].iloc[0]}")
 
   duckdb.sql(f"INSERT INTO output.hello_world VALUES ({context['block_number']}, 'Hello!')")
 
